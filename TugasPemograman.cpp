ce403006c7a1d0489f401106588f9d05b4c6458f
yafinoor <mnyafi777@gmail.com>
2017-12-31 12:54:10 +0800

    Menampilkan nilai X

--- TugasPemograman.cpp
+++ TugasPemograman.cpp
@@ -6,6 +6,12 @@ int main (){
 	cout.setf(ios::fixed);
 	cout.setf(ios::showpoint);
 	cout.precision(4);
+	
+		cout << "MATRIKS X : "<< endl;
+	double a1c[5]={4,4,5,3,3};
+	double a2c[5]={3,3,4,2,3};
+	double a3c[5]={5,4,2,2,2};
+	
 
 }
 	
