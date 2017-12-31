e339c0b80354772481c3faad76a684d289b43d02
yafinoor <mnyafi777@gmail.com>
2017-12-31 13:03:38 +0800

    Menampilkan Hasil Nilai Akhir

--- TugasPemograman.cpp
+++ TugasPemograman.cpp
@@ -59,6 +59,9 @@ int main (){
 	RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4],
 	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
 	RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]};
-
+	
+	cout <<"NIlai A1 : "<<ha[0]<<endl; 
+	cout <<"NIlai A2 : "<<ha[1]<<endl;
+	cout <<"NIlai A3 : "<<ha[2]<<endl;
 }
 	
