1197c507a08d540b6e4f5659d9e67d737cab1a77
yafinoor <mnyafi777@gmail.com>
2017-12-31 13:03:03 +0800

    Menampilkan Nilai Akhir

--- TugasPemograman.cpp
+++ TugasPemograman.cpp
@@ -54,6 +54,11 @@ int main (){
 	cout << RWa3c[0] <<" | "<<RWa3c[1]<<" | "<<RWa3c[2]<<" | "<<RWa3c[3]<<" | "<<RWa3c[4]<<endl;
 	cout << endl;
 	
+	cout << "HASIL AKHIR : "<< endl;
+	double ha[3]={
+	RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4],
+	RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4],
+	RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]};
 
 }
 	
